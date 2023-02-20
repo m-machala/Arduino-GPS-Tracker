db06d719b1735fef311de4411529f5262b020e3f
m-machala <114995208+m-machala@users.noreply.github.com>
2023-02-20 13:53:36 +0100

    add initial gpx functionality

--- src/GPX_generator/GPX_generator.h
+++ src/GPX_generator/GPX_generator.h
@@ -4,15 +4,17 @@
 namespace GPX_GEN {
     /*
         generates a skeleton for a GPX track
-        output - amount of bytes needed to reach the center from end
+        output - amount of bytes needed to reach the center from end, returns 0 if there is th buffer is too small
 
     */
     int generateSkeleton(char* buffer, int bufferSize, char* creator, char* trackName);
 
     /*
         generates a trackpoint
+        true - buffer filled successully
+        false - buffer too small
     */
-    void generateTrackpoint(float latitude, float longitude, float elevation, int year, int month, int day, int hour, int minute, int second);
+    bool generateTrackpoint(char* buffer, int bufferSize, float latitude, float longitude, float elevation, int year, int month, int day, int hour, int minute, int second);
 }
 
 #endif //GPX_GEN_H
