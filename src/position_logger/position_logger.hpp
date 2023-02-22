8dc7734d6dd0f4828869694484a696b2212bd798
m-machala <114995208+m-machala@users.noreply.github.com>
2023-02-22 10:08:57 +0100

    add position logger skeleton

---
+++ src/position_logger/position_logger.hpp
@@ -0,0 +1,21 @@
+#ifndef POS_LOG_H
+#define POS_LOG_H
+
+namespace POS_LOG {
+  /*
+    generates a .gpx file on the SD card and inserts the GPX skeleton into it
+  */
+  bool startLogging(const char* creator, const char* trackName);
+
+  /*
+    inserts a trackpoint into the current working .gpx file
+  */
+  bool addTrackpoint(float latitude, float longitude, float elevation, int year, int month, int day, int hour, int minute, int second);
+
+  /*
+    closes current working file, ends logging
+  */
+  void stopLogging();
+}
+
+#endif //POS_LOG_H
