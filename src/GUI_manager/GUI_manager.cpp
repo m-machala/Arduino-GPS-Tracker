d5d685842efa27cafdffd15d77f08887e5bb2e76
m-machala <114995208+m-machala@users.noreply.github.com>
2023-02-25 16:30:33 +0100

    remove old GUI code, add new GUI skeleton

--- src/GUI_manager/GUI_manager.cpp
+++ src/GUI_manager/GUI_manager.cpp
@@ -1,20 +1,2 @@
 #include "GUI_manager.hpp"
 #include "../bitmaps/bitmaps.hpp"
-
-Adafruit_SH1106G *GUI::display = new Adafruit_SH1106G(128, 64, &Wire, -1);
-
-void GUI::init() {
-  display->begin(0x3c, true); // i2c address
-  display->setTextSize(1);
-  display->setTextColor(SH110X_WHITE);
-  redraw();
-}
-
-void GUI::redraw() {
-  display->clearDisplay();
-  display->drawBitmap(117, 53, BM::Satellite, BM_Satellite_W, BM_Satellite_H, 1);
-  display->drawRect(0, 0, 128, 16, SH110X_WHITE);
-  display->drawLine(43, 0, 43, 15, SH110X_WHITE);
-  display->drawRect(0, 0, 128, 64, SH110X_WHITE);
-  display->display();
-}
--- src/GUI_manager/GUI_manager.hpp
+++ src/GUI_manager/GUI_manager.hpp
@@ -1,14 +1,61 @@
 #ifndef GUI_MAN_H
 #define GUI_MAN_H
 
-//#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SH110X.h>
-
 namespace GUI {
-  extern Adafruit_SH1106G *display;
-  void init();
-  void redraw();
+  /*
+    initialize display
+  */
+  bool init();
+
+  /*
+    displays custom splash screen
+  */
+  void splash();
+
+  /*
+    displays GUI background
+  */
+  void base();
+
+  /*
+    displays current date
+  */
+  void date(int year, int month, int day);
+
+  /*
+    displays current time
+  */
+  void time(int hour, int minute);
+
+  /*
+    displays the current amount of used satellites
+  */
+  void satCount(int count);
+
+  /*
+    displays a screen with an info message
+  */
+  void infoMessage(const char* message);
+
+  /*
+    displays a screen with an error message
+  */
+  void errorMessage(const char* message);
+
+  /*
+    displays starting screen with button prompt
+  */
+  void startScreen();
+
+  /*
+    displays screen with info about the current tracking session
+  */
+  void trackingScreen(int secondsSinceStart, float latitude, float longitude, int metersTraveled);
+
+  /*
+    displays screen with stats ebout ended session
+  */
+  void endScreen(int secondsSinceStart, int metersTraveled);
 }
 
 #endif //GUI_MAN_H  
